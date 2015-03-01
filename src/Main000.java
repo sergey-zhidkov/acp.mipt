199586047a2c39a17b0ce14372dbc9a4e96e115a
mobileDeveloper <4mobile.developer@gmail.com>
2015-03-01 03:35:03 +0300

    Next new problems

--- src/Main000.java
+++ src/Main000.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Main {
+class Main000 {
     public static void main(String args[]){
         Scanner s = new Scanner(System.in);
         System.out.println(s.nextInt()+s.nextInt());
---
+++ src/Main403.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main403 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String expression = br.readLine().trim();
+
+        int index = 1;
+        for (int i = index; i < expression.length(); i++) {
+            char ch = expression.charAt(i);
+            if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+                index = i;
+                break;
+            }
+        }
+
+        int first = Integer.parseInt(expression.substring(0, index).trim());
+        char operand = expression.charAt(index);
+        int second = Integer.parseInt(expression.substring(index + 1).trim());
+
+        int result = 0;
+        switch (operand) {
+            case '+': result = first + second; break;
+            case '-': result = first - second; break;
+            case '*': result = first * second; break;
+            case '/': result = first / second; break;
+        }
+
+        System.out.println(result);
+    }
+}
+
